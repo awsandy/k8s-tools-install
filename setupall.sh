e83dad24b9b003ebb223028d6f2755ebcdc46696
awsandy <awsandy@amazon.co.uk>
2020-05-15 14:29:55 +0100

    fixes

--- setupall.sh
+++ setupall.sh
@@ -31,7 +31,8 @@ echo "ssh key"
 mkdir -p ~/.ssh
 ssh-keygen -b 2048 -t rsa -f ~/.ssh/id_rsa -q -N ""
 chmod 600 ~/.ssh/id*
-aws ec2 import-key-pair --key-name "eksworkshop" --public-key-material file://~/.ssh/id_rsa.pub
+aws ec2 delete-key-pair --key-name "eksworkshop"
+aws ec2 import-key-pair --key-name "eksworkshop" --public-key-material fileb://~/.ssh/id_rsa.pub
 echo "KMS key"
 aws kms create-alias --alias-name alias/eksworkshop --target-key-id $(aws kms create-key --query KeyMetadata.Arn --output text)
 export MASTER_ARN=$(aws kms describe-key --key-id alias/eksworkshop --query KeyMetadata.Arn --output text)
