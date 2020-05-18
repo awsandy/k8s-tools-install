be5436e147f7d62bea5811dbb817a39036263e1d
awsandy <awsandy@amazon.co.uk>
2020-05-18 13:02:42 +0100

    version

--- k8s-versions.sh
+++ k8s-versions.sh
@@ -5,4 +5,4 @@ kubectl describe deployment coredns --namespace kube-system | grep Image | cut -
 echo "cni 1.6.1+"
 kubectl describe daemonset aws-node --namespace kube-system | grep Image | cut -d "/" -f 2
 echo "KubeProxy 1.16.8+"
-kubectl get daemonset kube-proxy --namespace kube-system -o=jsonpath='{$.spec.template.spec.containers[:1].image}'
+kubectl get daemonset kube-proxy --namespace kube-system -o=jsonpath='{$.spec.template.spec.containers[:1].image}' | cut -f2 -d':'
