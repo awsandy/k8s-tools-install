90e5db443661bcc63288ae5e5799471106a3d3e3
awsandy <awsandy@amazon.co.uk>
2020-05-18 13:09:32 +0100

    check

--- check.sh
+++ check.sh
@@ -1,3 +1,5 @@
+kubectl get configmap -n kube-system aws-auth -o yaml > /tmp/aws-auth.yaml
+cat /tmp/aws-auth.yaml
 echo "Check my profile"
 instid=`curl -s -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/instance-id`
 echo $instid
@@ -10,5 +12,3 @@ else
 echo "OK Found Instance profile eksworkshop-admin"
 fi
 aws sts get-caller-identity --query Arn | grep eksworkshop-admin -q && echo "IAM role valid - eksworkshop-admin" || echo "IAM role NOT valid"
-kubectl get configmap -n kube-system aws-auth -o yaml > /tmp/aws-auth.yaml
-cat /tmp/aws-auth.yaml
