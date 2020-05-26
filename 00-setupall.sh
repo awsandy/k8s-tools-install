8a083b5880b51867bdf6269cf389b115c749be48
awsandy <awsandy@amazon.co.uk>
2020-05-26 18:26:01 +0100

    helm repo

--- 00-setupall.sh
+++ 00-setupall.sh
@@ -50,6 +50,8 @@ wget https://get.helm.sh/helm-v3.2.1-linux-amd64.tar.gz
 tar -zxvf helm-v3.2.1-linux-amd64.tar.gz
 sudo mv linux-amd64/helm /usr/local/bin/helm
 rm -f helm-v3.2.1-linux-amd64.tar.gz linux-amd64
+echo "add a repo"
+helm repo add stable https://kubernetes-charts.storage.googleapis.com/
 
 
 echo "kubectx"
